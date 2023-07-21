7eae13ca43338484a1f5d78f697113f51985b165
Zhoomart Cholponbek uulu <ma4akk@gmail.com>
2023-07-21 16:44:47 +0600

    Refactored accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import authenticate
 from django.utils import timezone
 from rest_framework import status, generics, exceptions, response
-from .serializers import LoginSerializer, LogoutSerializer, UserSerializer, RegistrationSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import LoginSerializer, UserSerializer, RegistrationSerializer
 
 
 class LoginView(generics.GenericAPIView):
@@ -12,10 +13,11 @@ class LoginView(generics.GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid()
+        serializer.is_valid(raise_exception=True)
         user = authenticate(**serializer.validated_data)
         if not user:
             raise exceptions.AuthenticationFailed()
+
         user.last_login = timezone.now()
         user.save()
 
@@ -32,25 +34,11 @@ class LogoutView(generics.GenericAPIView):
     authentication_classes = ()
     permission_classes = ()
 
-    # serializer_class = LogoutSerializer
-
     def dispatch(self, request, *args, **kwargs):
-        response = super(LogoutView, self).dispatch(request, *args, **kwargs)
+        response = super().dispatch(request, *args, **kwargs)
         response.delete_cookie('jwt_refresh')
         response.delete_cookie('jwt_access')
         return response
-
-    # def post(self, request, *args, **kwargs):
-    #     # serializer = self.get_serializer(data=request.data)
-    #     # serializer.is_valid()
-    #     #
-    #     # access_token = serializer.validated_data["access_token"]
-    #     # refresh_token = serializer.validated_data["refresh_token"]
-    #     #
-    #     # BlacklistToken(access_token)
-    #     # BlacklistToken(refresh_token)
-    #
-    #     return response.Response(data={"message": "Logout successful"}, status=status.HTTP_200_OK)
 
 
 class RegistrationView(generics.GenericAPIView):
@@ -62,6 +50,7 @@ class RegistrationView(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+
         refresh = RefreshToken.for_user(user)
         data = {
             "user": UserSerializer(instance=user, context={"request": request}).data,
